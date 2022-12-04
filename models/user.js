bee4db82aaa17a3c4cc46b6d74abadeea78c53c0
Christian Luna <christianlunaduenas@gmail.com>
2022-12-04 07:27:07 -0800

    Setting up handlebar files. Also implementing comments on my worked files.

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+// User model created. Checking the password.
 class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
@@ -37,6 +38,7 @@ User.init(
     },
   },
   {
+    // "hooks" functionality to cycle secure password.
     hooks: {
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
